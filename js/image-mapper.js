b6790d5cacc9e9948ee928754cb5fede444fdf90
Christian Flores <christianedit@gmail.com>
2014-07-15 12:18:51 -0400

    cleanup, add upload, fix issues 3,4,7

--- js/image-mapper.js
+++ js/image-mapper.js
@@ -24,7 +24,7 @@
 $(document).ready(function() {
 	'use strict';
 
-// Cache some variables; others as reference.
+	// Cache some variables; others as reference.
 	var $image = $('#my-image'),
 			$canvas = $('#my-canvas'),
 			$hotspot_input = $('.hotspot-input'),
@@ -48,12 +48,12 @@ $(document).ready(function() {
 		// Push new coords into all coordinates array
 		coordinates.push((x_coord) + ',' + (y_coord));
 
-		// console.log(coordinates); // Sample output of coords
+		console.log(coordinates); // Sample output of coords
 
 		// Normalize markup and inject it
 		var map_output = format_map_output();
 		var canvas_output = format_canvas_output();
-		// Textarea is a mistress of her own kind. Using this 1-2 combo to insert markup.
+		// Display output in textarea
 		$('textarea').val(canvas_output + '\n\n' +  map_output);
 
 		draw_canvas(x_coord, y_coord);
@@ -69,54 +69,23 @@ $(document).ready(function() {
 
 	$canvas.dblclick(function(event) {
 
-		// set true so we can handle this later
 		double_click = true;
+
 		// end current canvas path
 		context.closePath();
 
 		console.log(coordinates); // for reference
-
+		
 		// clear coordinates array to start new one
 		coordinates = [];
-
+		console.log("coordinates prototype = " + coordinates);
 		console.log('double_click = ' + double_click); // for reference
 
 		return double_click; // exit event handler and return true
 
 	});
 
-	/*
-	* This needs to be built out and properly integrated.
-	* When user double clicks, we want to...
-	* - Finish drawing their path by connect begin && end points
-	* - End current canvas path 'closePath();'
-	* - Prepare for potential new path
-	* - And finally begin async embed of IMG data
-	*/
 	
-	/*
-		*	canvas.addEventListener('click', function(){
-		*		// Increment click counter by +1 to register single click.
-		* 	click_count++;
-		*		// Detect click, wait x seconds to see if a second click occurs.
-		* 	if (click_count === 1) {
-		* 		setTimeout(function(){
-		* 			click_count = 0;
-		* 		}, 250);
-		* 	}
-		*		// If second click occurs, register this.
-		* 	else if (click_count === 2) {
-		* 		double_click = true;
-		* 		click_count = 0;
-		* 		console.log('double click = ' + double_click);
-		* 		context.closePath();
-		* 		return double_click;
-		* 	}
-
-		* }, false);
-	*/
-
-
 	function format_map_output() {
 
 		// Reference vars to create markup
@@ -175,10 +144,10 @@ $(document).ready(function() {
 	}
 
 	/*
-	* Start of UI effects
-	* Keeping UI seperate until I have time to properly integrate
+	* Starts UI related stuff
 	*/
 
+	// url input UI
 	$canvas.dblclick(function(event) {
 		// reset input value to blank, display it, focus it.
 		$hotspot_input_field.val('');
---
+++ js/image-upload.js
@@ -0,0 +1,36 @@
+/*
+*	Upload local image and display it on same page
+*/
+
+
+
+(function(){
+	
+	var userImg = document.getElementById( "user-img" );
+	var imgContainer = document.getElementById( "my-image" );
+	
+
+	putImg = function() {
+		imgContainer.src = window.localStorage.getItem( "image-base64" );
+	},
+
+	/* Listener */
+	bindUi = function(){
+		userImg.addEventListener( "change", function(){
+		if ( this.files.length ) {
+			var reader = new FileReader();
+			reader.onload = function( e ){
+				window.localStorage.setItem( "image-base64", e.target.result );
+				putImg();
+			};
+			reader.readAsDataURL( this.files[ 0 ] );
+		}
+
+	}, false );
+
+	};
+
+	putImg();
+	bindUi();
+
+}());
